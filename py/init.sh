2e0bd296f50b6882aa0ac150629938cb0a47a72c
Kouta@314 <newtons.boiled.clock@gmail.com>
2022-01-23 14:24:21 +0900

    pythonライブラリをインストールするコマンドを追加

--- py/init.sh
+++ py/init.sh
@@ -1,2 +1,7 @@
 echo "Start" > hoge.txt
+echo date >> hoge.txt
+python3 -m pip install --upgrade pip
+echo date >> hoge.txt
+python3 -m pip install -r requirements.txt
+echo date >> hoge.txt
 echo "End" >> hoge.txt
